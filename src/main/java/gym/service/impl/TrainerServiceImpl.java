7cf184cbde1e01bc9e090bf5211802b73cb131f6
umidov <enterpricedeveloper@gmail.com>
2026-06-21 21:17:22 +0500

    adds logs to Service layer

--- src/main/java/gym/service/impl/TrainerServiceImpl.java
+++ src/main/java/gym/service/impl/TrainerServiceImpl.java
@@ -3,28 +3,38 @@ package gym.service.impl;
 import gym.dao.TrainerDao;
 import gym.domain.Trainer;
 import gym.service.TrainerService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TrainerServiceImpl implements TrainerService {
-    private final TrainerDao trainerDao;
+    private static final Logger log = LoggerFactory.getLogger(TrainerServiceImpl.class);
+    private TrainerDao trainerDao;
 
-    public TrainerServiceImpl(TrainerDao trainerDao) {
+    @Autowired
+    public void setTrainerDao(TrainerDao trainerDao) {
         this.trainerDao = trainerDao;
     }
 
     @Override
     public Trainer saveTrainer(Trainer trainer) {
-        return trainerDao.saveTrainer(trainer);
+        log.info("Creating trainer: {} {}", trainer.getFirstName(), trainer.getLastName());
+        Trainer saved = trainerDao.saveTrainer(trainer);
+        log.info("Created trainer with id={}", saved.getUserId());
+        return saved;
     }
 
     @Override
     public Trainer getTrainerById(Long id) {
+        log.info("Fetching trainer with id={}", id);
         return trainerDao.getTrainerById(id);
     }
 
     @Override
     public Trainer updateTrainer(Trainer trainer) {
+        log.info("Updating trainer with id={}", trainer.getUserId());
         return trainerDao.updateTrainer(trainer);
     }
 }
--- src/main/java/gym/service/impl/TrainingServiceImpl.java
+++ src/main/java/gym/service/impl/TrainingServiceImpl.java
@@ -3,23 +3,34 @@ package gym.service.impl;
 import gym.dao.TrainingDao;
 import gym.domain.Training;
 import gym.service.TrainingService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TrainingServiceImpl implements TrainingService {
-    private final TrainingDao trainingDao;
 
-    public TrainingServiceImpl(TrainingDao trainingDao) {
+    private static final Logger log = LoggerFactory.getLogger(TrainingServiceImpl.class);
+
+    private TrainingDao trainingDao;
+
+    @Autowired
+    public void setTrainingDao(TrainingDao trainingDao) {
         this.trainingDao = trainingDao;
     }
 
     @Override
     public Training saveTraining(Training training) {
-        return trainingDao.saveTraining(training);
+        log.info("Creating training: {}", training.getTrainingName());
+        Training saved = trainingDao.saveTraining(training);
+        log.info("Created training with id={}", saved.getId());
+        return saved;
     }
 
     @Override
     public Training getTrainingById(Long id) {
+        log.info("Fetching training with id={}", id);
         return trainingDao.getTrainingById(id);
     }
 }
